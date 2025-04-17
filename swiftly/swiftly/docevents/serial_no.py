fcddf4a5d7d759de7cbbeb5c75cf983d9f7871da
viralkansodiya <viral@fosserp.com>
2025-04-17 06:25:03 +0000

    updated the fixed

--- swiftly/swiftly/docevents/serial_no.py
+++ swiftly/swiftly/docevents/serial_no.py
@@ -1,96 +1,110 @@
 import frappe
 from frappe.utils.pdf import get_pdf
-from frappe.utils.file_manager import save_file, save_file_on_filesystem
+from frappe.utils.file_manager import save_file_on_filesystem
 import json
+import pyqrcode
+from frappe.utils import get_datetime
 
 @frappe.whitelist()
 def generate_serial_no_pdf(serial_nos):
     serial_nos = json.loads(serial_nos) if isinstance(serial_nos, str) else serial_nos
-    # Fetch print format HTML
-    
-    html_print_format = """
-                        
-                    <div class="update">
-                       
-                        <br>
-                        <table width="100%"  style="margin:0;">
-                            <tr>
-                                <td align="center"  style="margin:0;" colspan="2">
-                                    <p style="padding-bottom:0px; !important;"><b>Item Code: {0}</b></p>
-                                    <p><b>Serial No: {1}</b></p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td align="center" style="margin:0px">
-                                    <span style="text-align:center;" align="center"><img alt="" src="data:image/png;base64,{2}"></span>
-                                </td>
-                                <td align="center" style="margin:0px">
-                                    <span style="text-align:center;" align="center"><img alt="" src="data:image/png;base64,{3}"></span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="page-break"></div>
+    if len(serial_nos) < 21:
+        file = generate_qr(serial_nos)
+        return file
+    else:
+        frappe.enqueue(
+            generate_qr,
+            queue="default",
+            serial_nos= serial_nos
+        )
 
-    """
+def generate_qr(serial_nos):
+    
+    def get_qr_code(qr_text, scale=5):
+        return pyqrcode.create(qr_text).png_as_base64_str(scale=scale, quiet_zone=1)
+
     html_content = ""
+    html_template = """
+    <div class="update">
+        <table width="100%" style="margin:0;">
+            <tr>
+                <td align="center" colspan="2">
+                    <p><b>Item Code: {item_code}</b></p>
+                    <p><b>Serial No: {serial_no}</b></p>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" width="40%">
+                    <img src="data:image/png;base64,{qr_serial}" alt="QR Serial">
+                </td>
+                <td align="center" width="60%">
+                    <img src="data:image/png;base64,{qr_item}" alt="QR Item">
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="page-break"></div>
+    """
+
     for serial_no in serial_nos:
         doc = frappe.get_doc("Serial No", serial_no)
-        html_print_format = html_print_format.format(
-            doc.item_code, doc.name, get_qr_code(doc.name, scale=6), get_qr_code(doc.item_code, scale=5)
+        html_content += html_template.format(
+            item_code=doc.item_code,
+            serial_no=doc.name,
+            qr_serial=get_qr_code(doc.name, scale=6),
+            qr_item=get_qr_code(doc.item_code, scale=5)
         )
-        # Pass the full document instead of separate fields
-        final_content = frappe.render_template(html_print_format, {"doc": {'name':doc.name, "item_code":doc.item_code }}) 
-        html_content += final_content
-        html_content += "<div class='page-break'></div>"
 
-    # Convert HTML to PDF
-        html_content        +="""
-                <style>
-                    @page {size: 40mm 30mm; margin: 0;
-                    }
+    html_content += """
+    <style>
+        @page {size: 40mm 30mm; margin: 0;}
+        .page-break {page-break-after: always;}
+        p {font-size: 26px;}
+        td {text-align: center; vertical-align: middle; margin: 0;}
+        img {display: block; margin: auto;}
+    </style>
+    """
 
-                    .print-format td {
-                        padding : 0px !important;
-                    }
-                    
-                    .page-break {
-                        page-break-after: always;
-                    }
-                    
-                    p{
-                        font-size:26px;
-                    }
-                        body:last-child .print-format td img {
-                        width: 60% !important;
-                    }
-                    td {
-                        text-align: center !important; 
-                        vertical-align: middle; 
-                    }
-                    td img {
-                        display: block;
-                        margin: auto; 
-                    }
-
-
-                    </style>"""
     pdf_options = {
-        "page-width": "150mm",
-        "page-height": "130mm",
-        "margin-top": "0mm",
-        "margin-bottom": "0mm",
-        "margin-right": "0mm",
-        "margin-left": "0mm",
+        "page-width": "140mm",
+        "page-height": "110mm",
+        "margin-top": "0",
+        "margin-bottom": "0",
+        "margin-right": "0",
+        "margin-left": "0",
         "encoding": "UTF-8",
-        "orientation": "Portrait"  
     }
-    pdf_content = get_pdf(html_content, options=pdf_options)
+    if len(serial_nos) < 21:
+        pdf_content = get_pdf(html_content, options=pdf_options)
+        file_doc = save_file_on_filesystem(f"serial_no{str(get_datetime())}.pdf", pdf_content, is_private=0)
 
-    file_doc = save_file_on_filesystem("Selected_Serial_Nos.pdf", pdf_content, is_private=0)
+        return file_doc.get("file_url")
     
-    return file_doc.get("file_url")  
+    else:
+        content = get_pdf(html_content, options=pdf_options)
+        file_doc = save_file_on_filesystem(f"serial_no{str(get_datetime())}.pdf", content=content, is_private=0)
+        # doc = frappe.new_doc("File", )
+        # file_doc.save(ignore_permissions=True)
 
-import pyqrcode
-def get_qr_code(qr_text, scale=5):
-    return pyqrcode.create(qr_text).png_as_base64_str(scale=scale, quiet_zone=1)
+        # file_path = frappe.get_site_path("public", file_doc.file_url.lstrip("/"))
+
+        # Read and encode the file content
+        # with open(file_path, "rb") as f:
+        #     file_content = f.read()
+
+        # encoded_content = base64.b64encode(file_content).decode()
+
+        # Prepare the attachment
+        attachment = {
+            "fname": file_doc.get("file_name"),
+            "fcontent": content
+        }
+
+        # Send email
+        frappe.sendmail(
+            recipients=[frappe.session.user],
+            subject="Here is your QR Code PDF",
+            message="Please find the attached file.",
+            attachments=[attachment],
+            now=True
+        )
